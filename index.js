5873e3c2f89600873d55b5082df106a754935edd
Jared Noble <jsnoble1@gmail.com>
2017-08-11 14:43:30 -0700

    fixed different memory issues in code resolves# 10

--- index.js
+++ index.js
@@ -3,58 +3,59 @@
 var Promise = require("bluebird");
 
 function newReader(context, opConfig, jobConfig) {
-    var consumer_ready = false;
-    var subscribed = false;
-    var consumer;
     var events = context.foundation.getEventEmitter();
-
-    return function(partition, logger) {
-        if (! consumer_ready) {
-            consumer = context.foundation.getConnection({
-                type: "kafka",
-                endpoint: opConfig.connection,
-                options: {
-                    type: "consumer",
-                    group: opConfig.group
-                }
-            }).client;
-
-            consumer.on('ready', function() {
-                consumer_ready = true;
-
-                logger.info("Consumer ready");
-            });
+    var jobLogger = context.logger;
+    var consumer = context.foundation.getConnection({
+        type: "kafka",
+        endpoint: opConfig.connection,
+        options: {
+            type: "consumer",
+            group: opConfig.group
         }
+    }).client;
 
-        // We have to wait for the consumer to be ready. After the
-        // first slice this usually isn't an issue.
-        var subscriber = setInterval(function() {
-            if (consumer_ready && ! subscribed) {
-                logger.info("Subscribing to topic " + opConfig.topic);
-                clearInterval(subscriber);
-                consumer.subscribe([opConfig.topic]);
-                subscribed = true;
-            }
-        }, 10);
+    return new Promise(function(resolve, reject) {
+        consumer.on('ready', function() {
+            jobLogger.info("Consumer ready");
+            consumer.subscribe([opConfig.topic]);
+            resolve(processSlice)
+        });
+    });
 
+    function processSlice(partition, logger) {
         return new Promise(function(resolve, reject) {
+            var consuming = setInterval(consume, opConfig.interval);
             var slice = [];
             var iteration_start = Date.now();
 
-            events.on('worker:shutdown', function() {
-                consumer.disconnect();
-                resolve(slice);
-            });
+            consumer.on('data', receiveData);
+            consumer.on('error', error);
 
-            function completeSlice() {
+            events.on('worker:shutdown', finishSlice);
 
+            // This is going to have an issue that data will still be coming in
+            // while the chunk is being processed.
+
+            // Kick of initial processing.
+            consume();
+
+
+            function clearListeners() {
                 clearInterval(consuming);
-
                 consumer.removeListener('data', receiveData);
                 consumer.removeListener('error', error);
+                events.removeListener('worker:shutdown', finishSlice);
+            }
 
-                logger.info("Resolving with " + slice.length + " results.");
+            function finishSlice() {
+                clearListeners();
+                consumer.disconnect();
+                resolve(slice);
+            }
 
+            function completeSlice() {
+                clearListeners();
+                logger.warn(`Resolving with ${slice.length} results`);
                 resolve(slice);
             }
 
@@ -68,30 +69,19 @@ function newReader(context, opConfig, jobConfig) {
 
             function error(err) {
                 logger.error(err);
+                clearListeners();
                 reject(err);
             }
 
             function consume() {
-                if (subscribed) {
-                    if (((Date.now() - iteration_start) > opConfig.wait) || (slice.length >= opConfig.size)) {
-                        completeSlice();
-                    }
-                    else {
-                        consumer.consume(opConfig.size - slice.length);
-                    }
+                if (((Date.now() - iteration_start) > opConfig.wait) || (slice.length >= opConfig.size)) {
+                    completeSlice();
+                }
+                else {
+                    consumer.consume(opConfig.size - slice.length);
                 }
             }
 
-            var consuming = setInterval(consume, opConfig.interval)
-
-            // This is going to have an issue that data will still be coming in
-            // while the chunk is being processed.
-            consumer.on('data', receiveData);
-
-            consumer.on('error', error);
-
-            // Kick of initial processing.
-            consume();
         });
     }
 }
@@ -110,7 +100,7 @@ function newSlicer(context, job, retryData, slicerAnalytics, logger) {
     }]);
 }
 
-function schema(){
+function schema() {
     return {
         topic: {
             doc: 'Name of the Kafka topic to process',
